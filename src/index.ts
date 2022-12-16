6a2470c4d62c5f3c7904b0294a62873a0455d6cb
aless133 <aless133@yandex.ru>
2022-12-16 14:31:02 +0300

    feat: add webpack and build command

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import fsPromises from "node:fs/promises";
+import { some } from "./common";
+
 if (true) {
 	const c = 1;
 }
 fun1(1233);
+some();
+some();
+some();
+fsPromises.writeFile('wow', "hoho! "+(new Date()), { flag: "wx" });
+
 // fun1('qwe');
 
 function fun1(n: number) {
