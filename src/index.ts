6db4b14fdf61d99a716c317f9b2faf7b9658f57e
aless133 <aless133@yandex.ru>
2022-12-17 09:30:47 +0300

    feat: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import fsPromises from "node:fs/promises";
 import { some } from "./common";
 
 if (true) {
 	const c = 1;
 }
+console.log(process.env.PORT);
 fun1(1233);
 some();
 some();
 some();
-fsPromises.writeFile('wow', "hoho! "+(new Date()), { flag: "wx" });
+// fsPromises.writeFile("wow", "hoho! " + new Date(), { flag: "wx" });
 
 // fun1('qwe');
 
