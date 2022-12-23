5f829ee29d353aa2787a99990646dbb0bbc2da4a
Aless133 <aless133@yandex.ru>
2022-12-23 14:58:25 +0300

    feat: add multi.test

---
+++ test/multi.test.ts
@@ -0,0 +1,119 @@
+import { exec, spawn, ChildProcess } from "node:child_process";
+// import { cpus } from "node:child_process";
+import request from "supertest";
+import td from "./test.data";
+
+describe("Test Multi CRUD API, using http", () => {
+  let server = "http://localhost:4000",
+    serverProcess: ChildProcess;
+
+  // beforeAll(async () => {
+    // console.log("Starting the server...");
+
+    // exec('npm', (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error(`exec error: ${error}`);
+    //     throw new Error("server don't start");
+    //     return;
+    //   }
+    //   console.log(`stdout: ${stdout}`);
+    //   console.error(`stderr: ${stderr}`);
+    // });
+
+    // // serverProcess = spawn('npm', ['run', 'start']);
+    // // // serverProcess = spawn("npm", ["run", "start:multi0"]);
+    // // serverProcess.on("error", function (err) {
+    // //   console.error("Spawn error:", err);
+    // //   throw new Error("server don't start");
+    // // });
+    // // if (typeof serverProcess.pid !== "number") {
+    // //   console.error("spawn error");
+    // // } else {
+    //   await new Promise((resolve) => setTimeout(resolve, 1000));
+    // }
+  // });
+
+  // afterAll(() => {
+  //   console.log("Stopping the server...");
+  //   serverProcess.kill();
+  // });
+
+  test("getAll when empty", async () => {
+    const response = await request(server).get("/api/users");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([]);
+  });
+
+  test("create bad user", async () => {
+    const response = await request(server).post("/api/users").send(td.userBad1);
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("getAll after bad creation", async () => {
+    const response = await request(server).get("/api/users");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([]);
+  });
+
+  let id1 = "";
+  test("create new user 1", async () => {
+    const response = await request(server).post("/api/users").send(td.userData1);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ id: expect.any(String), ...td.userData1 });
+    id1 = response.body.id;
+  });
+
+  test("get user", async () => {
+    const response = await request(server).get(`/api/users/${id1}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ id: id1, ...td.userData1 });
+  });
+
+  let id2 = "";
+  test("create new user 2", async () => {
+    const response = await request(server).post("/api/users").send(td.userData1);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ id: expect.any(String), ...td.userData1 });
+    id2 = response.body.id;
+  });
+
+  test("getAll after 2 create", async () => {
+    const response = await request(server).get("/api/users");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveLength(2);
+  });
+
+  test("update user 1", async () => {
+    const response = await request(server).put(`/api/users/${id1}`).send(td.userData2);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ id: id1, ...td.userData2 });
+  });
+
+  test("get user 1 after update", async () => {
+    const response = await request(server).get(`/api/users/${id1}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ id: id1, ...td.userData2 });
+  });
+
+  test("delete user 1", async () => {
+    const response = await request(server).delete(`/api/users/${id1}`);
+    expect(response.statusCode).toBe(204);
+  });
+
+  test("getAll after 1 delete", async () => {
+    const response = await request(server).get("/api/users");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveLength(1);
+  });
+
+  test("get deleted user", async () => {
+    const response = await request(server).get(`/api/users/${id1}`);
+    expect(response.statusCode).toBe(404);
+  });
+
+  test("get the only user", async () => {
+    const response = await request(server).get(`/api/users/${id2}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ id: id2, ...td.userData1 });
+  });
+});
