d9f8348bb813dc0d129c5f44ff88d999b581bcfe
aless133 <aless133@yandex.ru>
2022-12-31 16:37:23 +0300

    refactor: primary use createProxy, not createServer

--- src/app/primary.ts
+++ src/app/primary.ts
@@ -1,7 +1,7 @@
 import cluster from "node:cluster";
 import { cpus } from "node:os";
 import { request, IncomingMessage } from "node:http";
-import { createServer } from "./server";
+import { createProxy } from "./server";
 import { dbCall } from "./db";
 import { getBody } from "./common";
 import { TRequestHandler, IApiReturn, IApiCall, IProcessMsg } from "./types";
@@ -11,7 +11,7 @@ let port = 0;
 export const initPrimary = (pport: number) => {
   port = pport;
   console.log(`Primary ${process.pid} is running on port :${port}`);
-  const server = createServer(port, balanceRequest);
+  const server = createProxy(port, balancerPort);
 
   // Fork workers.
   const numCPUs = cpus().length;
@@ -38,36 +38,14 @@ const balancer = {
   portCurrent: 0,
 };
 
-export const balancerSetup = ({ portBegin, portEnd }: { portBegin: number; portEnd: number }) => {
+const balancerSetup = ({ portBegin, portEnd }: { portBegin: number; portEnd: number }) => {
   balancer.portBegin = portBegin;
   balancer.portEnd = portEnd;
   balancer.portCurrent = portBegin;
 };
 
-export const balanceRequest: TRequestHandler = async (req): Promise<IApiReturn> => {
-  console.log("balance to :" + balancer.portCurrent);
-  const { method, url, headers } = req;
-  return new Promise(async (resolve) => {
-    const req1 = request(
-      {
-        method,
-        path: url,
-        headers,
-        port: balancer.portCurrent,
-      },
-      async (res1: IncomingMessage) => {
-        const res: IApiReturn = { code: res1.statusCode ?? 0, data: await getBody(res1) };
-        if (res1.headers["content-type"] === "application/json") {
-          res.data = JSON.parse(res.data as string); // что бы не было проблем с повторным декодированием
-        }
-        resolve(res);
-      },
-    );
-
-    balancer.portCurrent++;
-    if (balancer.portCurrent > balancer.portEnd) balancer.portCurrent = balancer.portBegin;
-
-    //finalizing new request with original body
-    req1.end(await getBody(req));
-  });
+export const balancerPort = () => {
+  const port = balancer.portCurrent++;
+  if (balancer.portCurrent > balancer.portEnd) balancer.portCurrent = balancer.portBegin;
+  return port;
 };
