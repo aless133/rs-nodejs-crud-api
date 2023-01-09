f2c921a5aae8e205b1b6f306c1f45f36c6aec607
aless133 <aless133@yandex.ru>
2023-01-10 00:30:39 +0300

    tests: add bad methods tests

--- test/bad.test.ts
+++ test/bad.test.ts
@@ -14,6 +14,11 @@ describe("Error/bad requests test CRUD API", () => {
     expect(response.statusCode).toBe(500);
   });
 
+  test("create on bad route", async () => {
+    const response = await request(server).post("/api/users/123").send(td.userData1);
+    expect(response.statusCode).toBe(404);
+  });
+
   test("create nojson user", async () => {
     const response = await request(server).post("/api/users").send("asdasda");
     expect(response.statusCode).toBe(400);
@@ -34,6 +39,11 @@ describe("Error/bad requests test CRUD API", () => {
     expect(response.statusCode).toBe(404);
   });
 
+  test("update on bad route", async () => {
+    const response = await request(server).put(`/api/users`).send(td.userData1);
+    expect(response.statusCode).toBe(404);
+  });
+
   test("update bad id user", async () => {
     const response = await request(server).put(`/api/users/${td.uuid1bad}`).send(td.userData1);
     expect(response.statusCode).toBe(400);
@@ -51,6 +61,11 @@ describe("Error/bad requests test CRUD API", () => {
 
   test("update not found user", async () => {
     const response = await request(server).put(`/api/users/${td.uuid2}`).send(td.userData1);
+    expect(response.statusCode).toBe(404);
+  });
+
+  test("delete on bad route", async () => {
+    const response = await request(server).delete(`/api/users`);
     expect(response.statusCode).toBe(404);
   });
 
